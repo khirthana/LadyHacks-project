000ec7a064ce246f5c09d0bb958aa5b5823d6d3a
khirthana <khirthana.subramanian@uoit.net>
2016-11-19 16:21:00 -0500

    updated database

---
+++ MyApplication/app/src/main/java/ladyhacks_project/ladyhacks_project/Product.java
@@ -0,0 +1,32 @@
+package ladyhacks_project.ladyhacks_project;
+
+
+public class Product {
+
+    int product_barcode;
+    String product_name;
+    String product_exp;
+
+
+    public Product (){    }
+
+    public Product (int product_id,String product_name, String product_exp){
+        this.product_barcode = product_id;
+        this.product_name = product_name;
+        this.product_exp = product_exp;
+    }
+
+    void setProduct_barcode(int id){this.product_barcode=id;}
+
+    void setProduct_name(String name){this.product_name=name;}
+
+    void setProduct_exp(String exp){this.product_exp=exp;}
+
+    public int getProduct_barcode(){return product_barcode;}
+
+    public String getProduct_name() {return product_name;}
+
+    public String getProduct_exp() {return product_exp;}
+
+}
+
---
+++ MyApplication/app/src/main/java/ladyhacks_project/ladyhacks_project/ProductDBHelper.java
@@ -0,0 +1,100 @@
+package ladyhacks_project.ladyhacks_project;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.*;
+
+public class ProductDBHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "product";
+    private static final String TABLE_PRODUCT_DETAIL = "productdetail";
+
+    private static final String KEY_id = "product_barcode";
+    private static final String KEY_name = "product_name";
+    private static final String KEY_exp = "product_exp";
+
+    public ProductDBHelper (Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+
+    }
+    public void onCreate (SQLiteDatabase db){
+        String CREATE_PRODUCT_TABLE = "CREATE TABLE " + TABLE_PRODUCT_DETAIL + "("
+                + KEY_id + " INTEGER PRIMARY KEY,"
+                + KEY_name + " TEXT,"
+                + KEY_exp+ " TEXT,";
+
+        db.execSQL(CREATE_PRODUCT_TABLE);
+    }
+
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCT_DETAIL);
+
+        onCreate(db);
+    }
+
+
+    //function for inserting a new product into the database
+    public boolean addData (int id, String name, String exp){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_id,id);
+        contentValues.put(KEY_name,name);
+        contentValues.put(KEY_exp,exp);
+        long result = db.insert (TABLE_PRODUCT_DETAIL, null, contentValues);
+
+        if (result == 1)
+            return false;
+        else
+            return true;
+    }
+
+    //function for querying the database, finding all products
+    public List<Product> getAllData (){
+        SQLiteDatabase db = this.getWritableDatabase();
+        //Cursor res = db.rawQuery("Select * From "+ TABLE_PRODUCT_DETAIL, null);
+        //return res;
+
+        ArrayList<Product> results = new ArrayList<>();
+
+        String[] columns = new String[] { KEY_id, KEY_name,KEY_exp};
+        String where = "";
+        String[] whereArgs = new String[] {};
+        String groupBy = "";
+        String groupArgs = "";
+        String orderBy =KEY_id;
+
+        Cursor cursor = db.query(TABLE_PRODUCT_DETAIL, columns, where, whereArgs,groupBy, groupArgs, orderBy);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()) {
+            int product_id=cursor.getInt(0);
+            String product_name= cursor.getString(1);
+            String product_exp=cursor.getString(2);
+
+            results.add(new Product(product_id,product_name,product_exp));
+
+            cursor.moveToNext();
+        }
+
+        return results;
+    }
+
+    //  function for deleting a product from the database
+    public Integer deleteData (String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_PRODUCT_DETAIL, "ID = ?", new String [] {id});
+    }
+}
